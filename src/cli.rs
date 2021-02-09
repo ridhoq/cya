85e25fb3545cf9bb675888e81b3253a74065f55e
Ridwan Hoq <ridhoq@users.noreply.github.com>
2021-02-08 23:31:37 -0600

    update doc string

--- src/cli.rs
+++ src/cli.rs
@@ -8,10 +8,10 @@ pub struct Opts {
     /// Number of requests to send to the HTTP URI under test
     #[clap(short, long, default_value = "1000")]
     pub requests: i32,
-    /// Number of workers. Controls parallelization of requests
+    /// Number of workers. Controls concurrency of requests. Not implemented yet
     #[clap(short, long, default_value = "4")]
     pub workers: i32,
-    /// HTTP URI to test
+    /// HTTP URI under test
     #[clap(required = true, name = "URI", parse(try_from_str))]
     pub uri: Uri,
 }
