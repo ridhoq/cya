f89669027cbfc504b0b1d93e00873dd829e8b5c7
Ridwan Hoq <ridhoq@users.noreply.github.com>
2021-02-09 23:21:52 -0600

    refactor to use reqwest

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,6 @@ use runner::run_test;
 #[tokio::main]
 async fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
-    run_test(opts.uri, opts.requests, opts.workers).await?;
+    run_test(opts.url, opts.requests, opts.workers).await?;
     Ok(())
 }
--- src/runner.rs
+++ src/runner.rs
@@ -1,33 +1,23 @@
 use anyhow::Result;
-use hyper::client::HttpConnector;
-use hyper::{Body, Client, Request, Uri};
-use hyper_tls::HttpsConnector;
+use reqwest::{Client, Request, Method, Url, Response};
 
-type CyaHttpsConnector = HttpsConnector<HttpConnector>;
-type CyaClient = Client<CyaHttpsConnector, Body>;
+fn get_user_agent() -> String {
+    format!("{} {}", clap::crate_name!(), clap::crate_version!())
+}
 
 /// Runs the load test
-pub async fn run_test(uri: Uri, requests: i32, _workers: i32) -> Result<()> {
-    let https = HttpsConnector::new();
-    let client = Client::builder().build::<CyaHttpsConnector, Body>(https);
+pub async fn run_test(url: Url, requests: i32, _workers: i32) -> Result<()> {
+    let client = Client::builder()
+        .user_agent(get_user_agent())
+        .build()?;
     for _ in 0..requests {
-        request(&client, &uri).await?;
+        request(&client, &url).await?;
     }
     Ok(())
 }
 
-pub async fn request(client: &CyaClient, uri: &Uri) -> Result<()> {
-    let req = Request::builder()
-        .method("GET")
-        .uri(uri)
-        .header(
-            "User-Agent",
-            format!("{} {}", clap::crate_name!(), clap::crate_version!()),
-        )
-        // TODO: couldn't figure out how to not pass a body with the Request::builder
-        // TODO: pass in a real body when doing POST/PUT/etc
-        .body(Body::from(""))
-        .unwrap();
-    client.request(req).await?;
+pub async fn request(client: &Client, url: &Url) -> Result<()> {
+    let req = client.request(Method::GET, &url.to_string());
+    req.send().await?;
     Ok(())
 }
