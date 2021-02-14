45ec78e7ad16ae7e03e99a7fe7145cd3a2cdb7ca
Ridwan Hoq <ridhoq@users.noreply.github.com>
2021-02-13 22:25:58 -0600

    set up CyaResult and record status codes

--- src/runner.rs
+++ src/runner.rs
@@ -1,10 +1,21 @@
 use anyhow::Result;
 use reqwest::{Client, Method, Url};
+use serde::Serialize;
+use std::collections::HashMap;
 use std::option::Option::Some;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use tokio::sync::mpsc;
 use tokio::task;
 use uuid::Uuid;
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct CyaResult {
+    succeeded: i32,
+    failed: i32,
+    response_codes: HashMap<String, i32>,
+    correlation_id: String,
+}
 
 fn get_user_agent(id: Uuid) -> String {
     format!("{}/{}/{}", clap::crate_name!(), clap::crate_version!(), id)
@@ -29,8 +40,6 @@ pub async fn run_test(url: Url, requests: i32, connections: i32) -> Result<()> {
             .build()?,
     );
     let url = Arc::new(url);
-    let succeeded = Arc::new(Mutex::new(0));
-    let failed = Arc::new(Mutex::new(0));
 
     println!("correlation id: {}", correlation_id);
 
@@ -53,26 +62,35 @@ pub async fn run_test(url: Url, requests: i32, connections: i32) -> Result<()> {
     });
 
     let receive_handle = task::spawn(async move {
+        let mut succeeded = 0;
+        let mut failed = 0;
+        let mut response_codes = HashMap::new();
+
         while let Some(handle) = reciever.recv().await {
             let result = handle.await.expect("oops");
             match result {
                 Ok(res) => {
+                    let response_code_count =
+                        response_codes.entry(res.status().to_string()).or_insert(0);
+                    *response_code_count += 1;
                     if res.status().is_success() {
-                        let mut succeeded = succeeded.lock().expect("Could not acquire lock");
-                        *succeeded += 1;
+                        succeeded += 1;
                     }
                 }
-                Err(_) => {
-                    let mut failed = failed.lock().expect("Could not acquire lock");
-                    *failed += 1;
+                Err(err) => {
+                    failed += 1;
                 }
             }
         }
 
-        let succeeded = succeeded.lock().unwrap();
-        let failed = failed.lock().unwrap();
-        println!("succeeded requests: {}", *succeeded);
-        println!("failed requests: {}", *failed);
+        let result = CyaResult {
+            succeeded,
+            failed,
+            response_codes,
+            correlation_id: correlation_id.to_string(),
+        };
+        let serialized = serde_json::to_string_pretty(&result).unwrap();
+        println!("{}", serialized);
     });
 
     receive_handle.await?;
