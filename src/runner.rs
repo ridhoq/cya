7ed8313527c14f20e80233856878e317f5e5cb3b
Ridwan Hoq <943533+ridhoq@users.noreply.github.com>
2021-02-13 18:33:10 -0600

    Use simple concurrency (#4)
    * refactor to use reqwest
    
    * it work but it ugly
    
    * collect succeded requests at the end of the receiver task
    
    * clean up clones - don't see a way around clones all the way down
    
    * make connections cli arg
    
    * add correlation id
    
    * count failed requests
    
    * rename arg
    
    * handle the sender error
    
    * move client to top scope, copy to thread scope
    
    * update docstring
    
    * fmt
    
    * 0.0.3
    
    Co-authored-by: Ridwan Hoq <ridhoq@users.noreply.github.com>

--- src/runner.rs
+++ src/runner.rs
@@ -1,33 +1,80 @@
 use anyhow::Result;
-use hyper::client::HttpConnector;
-use hyper::{Body, Client, Request, Uri};
-use hyper_tls::HttpsConnector;
+use reqwest::{Client, Method, Url};
+use std::option::Option::Some;
+use std::sync::{Arc, Mutex};
+use tokio::sync::mpsc;
+use tokio::task;
+use uuid::Uuid;
 
-type CyaHttpsConnector = HttpsConnector<HttpConnector>;
-type CyaClient = Client<CyaHttpsConnector, Body>;
-
-/// Runs the load test
-pub async fn run_test(uri: Uri, requests: i32, _workers: i32) -> Result<()> {
-    let https = HttpsConnector::new();
-    let client = Client::builder().build::<CyaHttpsConnector, Body>(https);
-    for _ in 0..requests {
-        request(&client, &uri).await?;
-    }
-    Ok(())
+fn get_user_agent(id: Uuid) -> String {
+    format!("{}/{}/{}", clap::crate_name!(), clap::crate_version!(), id)
 }
 
-pub async fn request(client: &CyaClient, uri: &Uri) -> Result<()> {
-    let req = Request::builder()
-        .method("GET")
-        .uri(uri)
-        .header(
-            "User-Agent",
-            format!("{} {}", clap::crate_name!(), clap::crate_version!()),
-        )
-        // TODO: couldn't figure out how to not pass a body with the Request::builder
-        // TODO: pass in a real body when doing POST/PUT/etc
-        .body(Body::from(""))
-        .unwrap();
-    client.request(req).await?;
+/// Runs the load test
+///
+/// The load tester generates load by spawning tasks that make the HTTP request.
+/// It creates a channel that is bounded by the connections argument which effectively controls the
+/// number of concurrent connections at any given time.
+///
+/// The sender task starts a task that makes the HTTP request and sends the handle of that task to
+/// the receiver task
+///
+/// The receiver task awaits the result of the HTTP request tasks and counts the number of successes
+/// and failures. In the future, this will do further aggregation of response status codes and durations.
+pub async fn run_test(url: Url, requests: i32, connections: i32) -> Result<()> {
+    let correlation_id = Uuid::new_v4();
+    let client = Arc::new(
+        Client::builder()
+            .user_agent(get_user_agent(correlation_id))
+            .build()?,
+    );
+    let url = Arc::new(url);
+    let succeeded = Arc::new(Mutex::new(0));
+    let failed = Arc::new(Mutex::new(0));
+
+    println!("correlation id: {}", correlation_id);
+
+    let (sender, mut reciever) = mpsc::channel(connections as usize);
+
+    task::spawn(async move {
+        for _ in 0..requests {
+            let client = Arc::clone(&client);
+            let url = Arc::clone(&url);
+            let handle = task::spawn(async move {
+                let client = Arc::clone(&client);
+                let url = Arc::clone(&url);
+                client.request(Method::GET, url.as_str()).send().await
+            });
+            sender
+                .send(handle)
+                .await
+                .expect("Failed to send to receiver");
+        }
+    });
+
+    let receive_handle = task::spawn(async move {
+        while let Some(handle) = reciever.recv().await {
+            let result = handle.await.expect("oops");
+            match result {
+                Ok(res) => {
+                    if res.status().is_success() {
+                        let mut succeeded = succeeded.lock().expect("Could not acquire lock");
+                        *succeeded += 1;
+                    }
+                }
+                Err(_) => {
+                    let mut failed = failed.lock().expect("Could not acquire lock");
+                    *failed += 1;
+                }
+            }
+        }
+
+        let succeeded = succeeded.lock().unwrap();
+        let failed = failed.lock().unwrap();
+        println!("succeeded requests: {}", *succeeded);
+        println!("failed requests: {}", *failed);
+    });
+
+    receive_handle.await?;
     Ok(())
 }
